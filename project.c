fcf4e0f3b503d49a7f5c68d3f01dd17c13fe1b86
Dan Wagner <danwagner@ksu.edu>
2018-04-16 01:32:25 +0000

    Individual sensors work; do ISRs work in Xenomai?

--- project.c
+++ project.c
@@ -35,6 +35,21 @@ void init_ir() {
     }
 }
 
+void read_encoder() {
+    last_enc_status = digitalRead(ENC_DT_PIN);
+    while(!digitalRead(ENC_CLK_PIN)) {// timing constraint here?
+        current_enc_status = digitalRead(ENC_DT_PIN);
+        flag = 1;
+    }
+
+    if (flag == 1) {
+        flag = 0;
+        if (last_enc_status  == 0 && current_enc_status == 1) count++;
+        else if (last_enc_status == 1 && current_enc_status == 0) count--;
+    }
+    printf("Encoder count: %d\n", count);
+}
+
 void init_encoder() {
 	if(wiringPiSetup() < 0){
 		fprintf(stderr, "Unable to setup wiringPi:%s\n",strerror(errno));
@@ -45,30 +60,22 @@ void init_encoder() {
 	pinMode(ENC_CLK_PIN, INPUT);
 
 	pullUpDnControl(ENC_SW_PIN, PUD_UP);
+    if (wiringPiISR(ENC_SW_PIN, INT_EDGE_FALLING, &read_encoder) , 0) {
+        printf("Unable to initialize ISR\n");
+    }
+    printf("End of init_encoder\n");
 }
 
-void read_encoder(void* arg) {
-    last_enc_status = digitalRead(ENC_DT_PIN);
-    while(!digitalRead(ENC_CLK_PIN)) {// timing constraint here?
-        current_enc_status = digitalRead(ENC_DT_PIN);
-        flag = 1;
-    }
 
-    if (flag == 1) {
-        flag = 0;
-        if (!last_enc_status && current_enc_status) count++;
-        if (last_enc_status && !current_enc_status) count--;
-    }
-    printf("Encoder count: %d\n", count);
-}
 int main(int argc, char* argv[]) {
     printf("Starting IR sensor task.\n");
     init_encoder();
-    init_ir();
+//    init_ir();
     // &task, name, stack size, priority, mode);
-    rt_task_create(&ir, "IR", 0, 50, 0); 
-    // causes issues; next set to a periodic task every 4 seconds
-    rt_task_set_periodic(&ir, TM_NOW, rt_timer_ns2ticks(4000000000));
+//    rt_task_create(&ir, "IR", 0, 50, 0); 
+
+    // causes issues; next set to a periodic task every 15 ms
+    //rt_task_set_periodic(&ir, TM_NOW, rt_timer_ns2ticks(15000));
     // rt_task_start(&ir, &readISR, 0);
     rt_task_create(&encoder, "Encoder", 1, 50, 0);
     rt_task_start(&encoder, &read_encoder, 0);
